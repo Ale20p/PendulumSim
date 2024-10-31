97b6ce45392ef1f7954daa001c83c5875a1a0a9e
Alessandro Pomponi <alescode1@gmail.com>
2024-10-31 13:23:50 -0400

    gutted the PPScreenHandler to adjust for rectangle string and change of length change functionality

--- src/main/java/pendulumsim/PPScreenHandler.java
+++ src/main/java/pendulumsim/PPScreenHandler.java
@@ -1,20 +1,21 @@
 package pendulumsim;
 
-import javafx.animation.Animation;
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
+import javafx.animation.*;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Line;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.transform.Rotate;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
@@ -25,14 +26,15 @@ import java.util.ResourceBundle;
 public class PPScreenHandler implements Initializable {
     private Stage mstage;
     @FXML AnchorPane PPSpane;
-        Line string = new Line();
+    @FXML Rectangle string;
     @FXML Circle ball;
     @FXML TextField lengthInput;
+    @FXML Slider lengthSlider;
     @FXML TextField amplitudeInput;
     @FXML TextField gravityInput;
-    @FXML TextField angularfrequencyInput;
     @FXML TextField massInput;
     @FXML Button backButton;
+    @FXML Pane pendulumHolder;
 
     private double length;
     private double gravity;
@@ -46,22 +48,18 @@ public class PPScreenHandler implements Initializable {
     }
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        string.endXProperty().bind(ball.centerXProperty());
-        string.endYProperty().bind(ball.centerYProperty());
+        int positonX = (int) (pendulumHolder.getPrefWidth()/2);
 
-        // Timeline animation which will update the pendulum's position every frame
-        Timeline animation = new Timeline(new KeyFrame(Duration.seconds(0.016), event -> {
-            time += 0.016;
+        Rotate rotation = new Rotate(75, positonX, 0);
+        pendulumHolder.getTransforms().add(rotation);
 
-        }));
+        KeyValue initkeyvalue = new KeyValue(rotation.angleProperty(),-75 , Interpolator.EASE_BOTH);
+        KeyFrame initkeyframe = new KeyFrame(Duration.seconds(6), initkeyvalue);
+        Timeline animation = new Timeline(initkeyframe);
+
+        animation.setAutoReverse(true);
         animation.setCycleCount(Timeline.INDEFINITE);
         animation.play();
-    }
-
-    private void updatePendulum() {
-        time += 0.016;
-
-        double theta = Equations.calculateDisplacement(amplitude, angularFrequency, time);
     }
 
     public void backToStartEvent() throws IOException {
@@ -72,6 +70,48 @@ public class PPScreenHandler implements Initializable {
         Scene scene = new Scene(root);
         mstage.setScene(scene);
     }
+
+    public void  startEvent() {
+        if (lengthInput.getText().isEmpty()) {
+            lengthInput.setText(String.valueOf(lengthSlider.getValue()));
+            length = lengthSlider.getValue();
+            string.setHeight(length);
+        } else {
+            length = Double.parseDouble(lengthInput.getText());
+            string.setHeight(length);
+        }
+
+        if (gravityInput.getText().isEmpty()) {
+            gravityInput.setText("9.8");
+            gravity = Double.parseDouble(gravityInput.getText());
+        } else {
+            gravity = Double.parseDouble(gravityInput.getText());
+        }
+
+        if (amplitudeInput.getText().isEmpty()) {
+            amplitudeInput.setText("0.1");
+            amplitude = Double.parseDouble(amplitudeInput.getText());
+        } else {
+            amplitude = Double.parseDouble(amplitudeInput.getText());
+        }
+
+        if (massInput.getText().isEmpty()) {
+            massInput.setText("0.5");
+            mass = Double.parseDouble(massInput.getText());
+        } else {
+            mass = Double.parseDouble(massInput.getText());
+        }
+    }
+
+    public void pauseEvent() throws IOException {
+
+    }
+
+    public void resetEvent() throws IOException {
+        ball.setCenterX(0);
+        ball.setCenterY(0);
+    }
+
     @FXML
     public void settingsevent() throws IOException {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("SettingsScreen.fxml"));
@@ -91,54 +131,5 @@ public class PPScreenHandler implements Initializable {
         Scene scene = new Scene(root);
         mstage.setScene(scene);
 
-    }
-
-    public void  startEvent() {
-        if (lengthInput.getText().isEmpty()) {
-            lengthInput.setText("1");
-            length = Double.parseDouble(lengthInput.getText());
-        } else {
-            length = Double.parseDouble(lengthInput.getText());
-            ball.setCenterY(string.getStartY() + length);
-        }
-
-        if (gravityInput.getText().isEmpty()) {
-            gravityInput.setText("9.8");
-            gravity = Double.parseDouble(gravityInput.getText());
-        } else {
-            gravity = Double.parseDouble(gravityInput.getText());
-        }
-
-        if (amplitudeInput.getText().isEmpty()) {
-            amplitudeInput.setText("0.1");
-            amplitude = Double.parseDouble(amplitudeInput.getText());
-        } else {
-            amplitude = Double.parseDouble(amplitudeInput.getText());
-        }
-
-        if (angularfrequencyInput.getText().isEmpty()) {
-            angularfrequencyInput.setText("100");
-            angularFrequency = Double.parseDouble(angularfrequencyInput.getText());
-        } else {
-            angularFrequency = Double.parseDouble(angularfrequencyInput.getText());
-        }
-
-        if (massInput.getText().isEmpty()) {
-            massInput.setText("0.5");
-            mass = Double.parseDouble(massInput.getText());
-        } else {
-            mass = Double.parseDouble(massInput.getText());
-        }
-
-
-    }
-
-    public void pauseEvent() throws IOException {
-
-    }
-
-    public void resetEvent() throws IOException {
-        ball.setCenterX(0);
-        ball.setCenterY(0);
     }
 }
