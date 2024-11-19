b2814e0ab03f360bab63b25c66b8ba55e4c2a259
Tristan Giannopoulos <tristan.giannopoulos@gmail.com>
2024-11-19 12:43:41 -0500

    Settings Work

--- src/main/java/pendulumsim/Controller/SettingsScreenHandler.java
+++ src/main/java/pendulumsim/Controller/SettingsScreenHandler.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.CheckBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -16,8 +17,14 @@ public class SettingsScreenHandler implements Initializable {
     public void setStage(Stage stage) {
         mstage = stage;
     }
+    @FXML CheckBox periodcheck;
+    @FXML CheckBox angularcheck;
+    @FXML CheckBox displacementcheck;
+    @FXML CheckBox velocitycheck;
+    @FXML CheckBox accelerationcheck;
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        
 
     }
 
